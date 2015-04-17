8615985aea39b4f9fe88137fae71b9b4f86ae39f
Sherkhan <sherkhan@xakep.ru>
2015-04-17 22:37:50 +0300

    new form

--- form/post.php
+++ form/post.php
@@ -1,14 +1,22 @@
 <?php
 // check if a form was submitted
 if( !empty( $_POST ) ){
+	$file = '';
+	echo "<pre>".print_r($_POST, true)."</pre>";
+	if (!empty($_POST['master'])) {
+		$file = $_POST['master'].'.json';
+		unset($_POST['master']);		
+	} else {
+		die();
+	}
 // convert form data to json format
 $json = json_encode( $_POST );
 // make sure there were no problems
 //if( json_last_error() != JSON_ERROR_NONE ){
     //exit;  // do your error handling here instead of exiting
 // }
-$file = 'entries.json';
 // write to file
 //   note: _server_ path, NOT "web address (url)"!
 file_put_contents( $file, $json, FILE_APPEND);
+echo 'ok';
 } ?>
